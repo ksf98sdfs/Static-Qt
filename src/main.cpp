3f55bd8b0038d89ea9b31ce7955daf4a6e169712
ksf98sdfs <kasdnoasi@outlook.com>
2024-09-30 21:14:53 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,7 @@
-#include <QApplication>
-#include <QPushButton>
-#include <QNetworkAccessManager>
-#include <QUrl>
-#include <QNetworkRequest>
-#include <QNetworkReply>
+#include <iostream>
 
-int main(int argc, char **argv)
+int main()
 {
- QApplication app (argc, argv);
-
- QWidget window;
- window.setFixedSize(100, 50);
-
- QPushButton *button = new QPushButton("Hello World", &window);
- button->setGeometry(10, 10, 80, 30);
-
- QNetworkAccessManager* manager = new QNetworkAccessManager(button);
- QNetworkRequest request(QUrl(QStringLiteral("https://www.svt.se/text-tv/100")));
- QNetworkReply* reply = manager->get(request);
- QObject::connect(reply, &QNetworkReply::finished, [button, reply]() {
-   reply->manager()->deleteLater();
-   reply->deleteLater();
-   button->setText(QString::number(reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt()));
- });
- 
- window.show();
- return app.exec();
+    std::cout << "Hello World" << std::endl;
+    return 0;
 }
